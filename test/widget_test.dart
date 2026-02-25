aaaf0303fa971e38d814661630dfc24e4823e9c0
lenovo <yousefmahmoud772@gmail.com>
2026-02-25 03:34:57 +0200

    add login files

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:transite_way/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const TransitWay());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
